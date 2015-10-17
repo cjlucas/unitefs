993779e1c4ba58848255729a93d4dd14320e20c8
Chris Lucas <chris@chrisjlucas.com>
2015-10-17 12:17:42 -0700

    wip

--- src/cjlucas.net/unitefs/main.go
+++ src/cjlucas.net/unitefs/main.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path"
-	"path/filepath"
-	"strings"
 
 	"cjlucas.net/unitefs/unitefs"
 
@@ -14,92 +11,16 @@ import (
 	"bazil.org/fuse/fs"
 )
 
-type HandleMap map[fuse.HandleID]*os.File
-
-var masterHandleMap HandleMap
-
-func (m HandleMap) Open(id fuse.HandleID, name string) (*os.File, error) {
-	if _, ok := m[id]; ok {
-		panic(fmt.Sprintf("Handle with id %d already present in map\n", id))
-	}
-
-	fp, err := os.Open(name)
-
-	if err == nil {
-		m[id] = fp
-	}
-
-	return fp, err
-}
-
-func (m HandleMap) Close(id fuse.HandleID) error {
-	return m[id].Close()
-}
-
-func (m HandleMap) ReadAt(id fuse.HandleID, offset int64, buf []byte) (int, error) {
-	if fp, ok := m[id]; ok {
-		return fp.ReadAt(buf, offset)
-	}
-
-	panic(fmt.Sprintf("Handle with id %d not found in map", id))
-}
-
-var tree fs.Tree
-var nextInode = uint64(2)
-
-func RootWalker(pathStr string, info os.FileInfo, err error) error {
-	pathStr = path.Clean(pathStr)
-	rootPath := path.Clean(os.Args[1])
-
-	if len(pathStr) == len(rootPath) {
-		tree.Root = unitefs.NewNode("/", 1)
-		tree.Root.Mode = os.ModeDir | 0777
-		tree.Nodes = make(NodeChildMap)
-		tree.Add(tree.Root)
-		return nil
-	}
-
-	pathRel := pathStr[len(rootPath)+1:]
-	pathSplit := strings.Split(pathRel, "/")
-
-	fmt.Println(pathSplit)
-
-	parent := tree.Root
-	for i := range pathSplit {
-		name := pathSplit[i]
-
-		if i == len(pathSplit)-1 {
-			node := unitefs.NewNode(name, nextInode)
-			node.Name = name
-			node.Mode = info.Mode()
-			node.Size = uint64(info.Size())
-			node.RealPath = pathStr
-
-			tree.Nodes[parent] = append(tree.Nodes[parent], node)
-			tree.Add(node)
-			nextInode++
-			break
-		}
-
-		for _, c := range tree.Nodes[parent] {
-			if name == c.Name {
-				parent = c
-			}
-		}
-	}
-
-	return nil
-}
-
 func main() {
-	src := path.Clean(os.Args[1])
-	masterHandleMap = make(HandleMap)
+	ufs := unitefs.NewFS()
 
-	filepath.Walk(src, RootWalker)
-	fmt.Println(tree)
+	for i := 1; i < len(os.Args)-1; i++ {
+		println(os.Args[i])
+		ufs.RegisterSubtree(os.Args[i])
+	}
 
 	c, err := fuse.Mount(
-		os.Args[2],
+		os.Args[len(os.Args)-1],
 		fuse.FSName("unitefs"),
 		fuse.Subtype("unitefs"),
 		fuse.LocalVolume(),
@@ -110,7 +31,9 @@ func main() {
 	}
 	defer c.Close()
 
-	err = fs.Serve(c, unitefs.FS{})
+	fmt.Println("FS mounted")
+
+	err = fs.Serve(c, ufs)
 	if err != nil {
 		log.Fatal(err)
 	}
--- src/cjlucas.net/unitefs/unitefs/fs.go
+++ src/cjlucas.net/unitefs/unitefs/fs.go
@@ -2,8 +2,10 @@ package unitefs
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"path"
+	"path/filepath"
 	"syscall"
 	"time"
 
@@ -12,15 +14,26 @@ import (
 	"golang.org/x/net/context"
 )
 
+type handleManager interface {
+	GetHandle(id fuse.HandleID) *os.File
+	RegisterHandle(id fuse.HandleID, fp *os.File)
+	UnregisterHandle(id fuse.HandleID)
+}
+
+type inodeManager interface {
+	NextInode() uint64
+}
+
 type Node struct {
-	Name     string
-	RealPath string
-	Inode    uint64
-	Size     uint64
-	Mtime    time.Time
-	Ctime    time.Time
-	Mode     os.FileMode
-	Tree     *Tree
+	Name          string
+	RealPath      string
+	Inode         uint64
+	Size          uint64
+	Mtime         time.Time
+	Ctime         time.Time
+	Mode          os.FileMode
+	Tree          *Tree
+	handleManager handleManager
 }
 
 func timeFromTimespec(ts syscall.Timespec) time.Time {
@@ -49,6 +62,20 @@ func (n Node) Children() []Node {
 	return n.Tree.Nodes[n]
 }
 
+func (n Node) asDirent() fuse.Dirent {
+	var d fuse.Dirent
+
+	d.Name = n.Name
+	d.Inode = n.Inode
+	if os.ModeDir&n.Mode > 0 {
+		d.Type = fuse.DT_Dir
+	} else {
+		d.Type = fuse.DT_File
+	}
+
+	return d
+}
+
 func (n Node) Attr(ctx context.Context, a *fuse.Attr) error {
 	a.Inode = n.Inode
 	a.Mode = n.Mode
@@ -68,26 +95,19 @@ func (n Node) Lookup(ctx context.Context, name string) (fs.Node, error) {
 
 func (n Node) Flush(ctx context.Context, req *fuse.FlushRequest) error {
 	fmt.Println("Flush")
-	/*
-	 *if fp, ok := masterHandleMap[req.Handle]; ok {
-	 *    delete(masterHandleMap, req.Handle)
-	 *    return fp.Close()
-	 *}
-	 */
+	n.handleManager.UnregisterHandle(req.Handle)
 
 	return nil
 }
 
 func (n Node) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
-	/*
-	 *if fp, ok := masterHandleMap[req.Handle]; fp == nil || !ok {
-	 *    masterHandleMap.Open(req.Handle, n.RealPath)
-	 *}
-	 */
+	if fp := n.handleManager.GetHandle(req.Handle); fp != nil {
+		resp.Data = make([]byte, req.Size)
+		_, err := fp.ReadAt(resp.Data, req.Offset)
+		return err
+	}
 
-	resp.Data = make([]byte, req.Size)
-	//masterHandleMap.ReadAt(req.Handle, req.Offset, resp.Data)
-	return nil
+	return syscall.EBADF
 }
 
 func (n Node) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
@@ -96,14 +116,7 @@ func (n Node) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 	dirs := make([]fuse.Dirent, len(children))
 
 	for i := range dirs {
-		c := children[i]
-		dirs[i].Name = c.Name
-		dirs[i].Inode = c.Inode
-		if os.ModeDir&c.Mode > 0 {
-			dirs[i].Type = fuse.DT_Dir
-		} else {
-			dirs[i].Type = fuse.DT_File
-		}
+		dirs[i] = children[i].asDirent()
 	}
 
 	fmt.Println(dirs)
@@ -113,22 +126,125 @@ func (n Node) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 type NodeChildMap map[Node][]Node
 
 type Tree struct {
-	Root  Node
-	Nodes NodeChildMap
+	Root          Node
+	Nodes         NodeChildMap
+	handleManager handleManager
+	inodeManager  inodeManager
+}
+
+func newTree(name string) *Tree {
+	t := Tree{}
+	t.Nodes = make(NodeChildMap)
+	return &t
 }
 
 func (t Tree) Add(node Node) {
 	t.Nodes[node] = make([]Node, 0)
 }
 
+func (t *Tree) walker(fpath string, info os.FileInfo, err error) error {
+	n, _ := newNodeForPath(fpath)
+	n.handleManager = t.handleManager
+	n.Tree = t
+	n.Inode = t.inodeManager.NextInode()
+
+	fmt.Printf("%#v\n", n)
+
+	return nil
+}
+
+func (t *Tree) getLevel(level int) []Node {
+	parents := []Node{t.Root}
+
+	for i := 0; i <= level; i++ {
+		children := make([]Node, 0)
+		for _, n := range parents {
+			for _, c := range n.Children() {
+				children = append(children, c)
+			}
+		}
+
+		parents = children
+	}
+
+	return parents
+}
+
 type UnionTree struct {
-	Trees      map[string]Tree
-	usedInodes []uint64
+	Tree
+
+	Subtrees map[string]*Tree
+}
+
+func (t *UnionTree) Build() error {
+	return nil
 }
 
 type FS struct {
-	UnionTree Tree
-	HandleMap map[fuse.HandleID]*os.File
+	UnionTree  UnionTree
+	handleMap  map[fuse.HandleID]*os.File
+	usedInodes []uint64
+}
+
+// Implement inodeManager
+func (fs *FS) NextInode() uint64 {
+	isUsed := func(n uint64) bool {
+		for _, inode := range fs.usedInodes {
+			if n == inode {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	for {
+		n := uint64(rand.Int63())
+		if !isUsed(n) {
+			return n
+		}
+	}
+
+}
+
+func NewFS() *FS {
+	ufs := FS{}
+	ufs.UnionTree.Subtrees = make(map[string]*Tree)
+	return &ufs
+}
+
+func (fs *FS) GetHandle(id fuse.HandleID) *os.File {
+	if fp := fs.handleMap[id]; fp != nil {
+		return fp
+	} else {
+		return nil
+	}
+}
+
+// Implement HandleManager
+func (fs *FS) RegisterHandle(id fuse.HandleID, fp *os.File) {
+	if _, ok := fs.handleMap[id]; ok {
+		panic(fmt.Sprintf("Handle with id %d already registered", id))
+	}
+
+	fs.handleMap[id] = fp
+}
+
+func (fs *FS) UnregisterHandle(id fuse.HandleID) {
+	if _, ok := fs.handleMap[id]; !ok {
+		panic(fmt.Sprintf("Handle with %d not registered", id))
+	}
+
+	delete(fs.handleMap, id)
+}
+
+func (fs *FS) RegisterSubtree(treeRoot string) {
+	treeRoot = path.Clean(treeRoot)
+	t := newTree(treeRoot)
+	t.inodeManager = fs
+	filepath.Walk(treeRoot, t.walker)
+
+	fs.UnionTree.Subtrees[treeRoot] = t
 }
 
 func (fs FS) Root() (fs.Node, error) {
